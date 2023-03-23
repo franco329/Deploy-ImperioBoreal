5837c2a8a32b329bfd6477bac441502ecb4bedfd
Fran <franco_kuperman@hotmail.com>
2023-03-23 20:47:44 -0300

    serverConfig

--- serverImperioBoreal/dbConnection.js
+++ serverImperioBoreal/dbConnection.js
@@ -6,9 +6,10 @@ const DB_PASSWORD = process.env.DB_PASSWORD;
 const CLUSTER = process.env.CLUSTER
 const DB_PORT = process.env.DB_PORT
 
+// mongodb+srv://<username>:<password>@cluster0.a9jnlqj.mongodb.net/test
 module.exports = async function connect() {
     try {
-        await mongoose.connect(`mongodb://${DB_USER}:${DB_PASSWORD}@${CLUSTER}:${DB_PORT}/`, {
+        await mongoose.connect(`mongodb+srv://${DB_USER}:${DB_PASSWORD}@${CLUSTER}.mongodb.net/ImperioBoreal`, {
             useNewUrlParser: true,
             useUnifiedTopology: true,
         });
