b8ce5733a7e07665dd011e4c8e6180143384866d
Fran <franco_kuperman@hotmail.com>
2023-03-25 22:35:01 -0300

    actualizando deploy client

--- clientImperioBoreal/src/Components/Navbar/Navbar.tsx
+++ clientImperioBoreal/src/Components/Navbar/Navbar.tsx
@@ -10,7 +10,7 @@ import { User } from "auth0";
 import { CartContext } from "../../context/index";
 import { CartContextType } from "../../types.d";
 
-import style from "./NavBar.module.css";
+import style from "./Navbar.module.css";
 //<------------------COMPONENTE NAVBAR---------------------->
 
 interface NavBarProps {
