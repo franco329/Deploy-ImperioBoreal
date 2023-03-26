1c4244affd221c071999c9c95ac4122c52418743
Fran <franco_kuperman@hotmail.com>
2023-03-25 23:08:32 -0300

    env base url sacada

--- clientImperioBoreal/src/App.tsx
+++ clientImperioBoreal/src/App.tsx
@@ -17,7 +17,7 @@ import Profile from "./Components/Profile/Profile";
 import { useAuth0 } from "@auth0/auth0-react";
 import Loading from './Components/Loading/Loading';
 import axios from 'axios';
-axios.defaults.baseURL = import.meta.env.VITE_BASE_URL;
+axios.defaults.baseURL = "https://deploy-imperioboreal-production.up.railway.app";
 
 //Si tienen problemas con el import en minuscula o mayuscula 
 //es por el cache de vscode, tienen que cerrar y volver a abrirlo
