bf4ecd3aafb133daa5b614568884c97da5c74872
Fran <franco_kuperman@hotmail.com>
2023-04-02 22:21:15 -0300

    paginado home arreglado

--- clientImperioBoreal/src/Components/Home/Home.tsx
+++ clientImperioBoreal/src/Components/Home/Home.tsx
@@ -75,8 +75,8 @@ const Home: React.FC = () => {
   };
   const pages: number[] = [];
 
-  for (let i = 1; i < Math.ceil(allProducts?.length / itemsPerPage); i++) {
-    pages.push(i);
+  for (let i = 0; i < Math.ceil(allProducts?.length / itemsPerPage); i++) {
+    pages.push(i + 1);
   }
 
   const indexOfLastItem = currentPage * itemsPerPage;
